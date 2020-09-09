7641e58fa4478b6df64f577fec155c9d8785ce8e
SamYuan1990 <yy19902439@126.com>
2020-09-09 22:18:31 +0800

    1st version for completed
    Signed-off-by: SamYuan1990 <yy19902439@126.com>

--- ci.sh
+++ ci.sh
@@ -5,6 +5,13 @@ docker build -t hfrd/minifab:latest .
 git clone https://github.com/SamYuan1990/stupid.git && cd stupid && git checkout docker
 docker build -t stupid:latest . && cd ..
 ./minifab up
-docker ps
-docker network ls
+docker run  --network minifab -v $PWD:/tmp  stupid stupid /tmp/config.yaml 100
+./minifab cleanup
+
+sleep 10
+
+rm spec.yaml
+mv spec2.yaml spec.yaml
+./minifab up
+docker run  --network minifab -v $PWD:/tmp  stupid stupid /tmp/config.yaml 100
 ./minifab cleanup
